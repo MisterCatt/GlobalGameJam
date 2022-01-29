4f541b0b4309deff8fe39ce1bcf4d66897aca8fa
Pastelgrill <indre.grigaraviciute.9295@student.uu.se>
2022-01-29 14:22:38 +0100

    fixed sacle stuff idk

--- GlobalGameJamFishGame/Assets/Scripts/AmmountCheck.cs
+++ GlobalGameJamFishGame/Assets/Scripts/AmmountCheck.cs
@@ -1,52 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AmmountCheck : MonoBehaviour
 {
-   public Sprite newSprite1, newSprite2, newSprite3,newSprite4,newSprite5;
-    public int AmmountCollected = 0;
-    GameObject obj;
-    Animator animator;
-    SpriteRenderer renderer;
-    Transform transform;
-    int lastAmmount=0;
+    public Sprite newSprite1, newSprite2, newSprite3, newSprite4, newSprite5;
+    public Sprite newSprite6, newSprite7, newSprite8, newSprite9, newSprite10;
+    public int AmmountCollectedBlue, AmmountCollectedOrange;
+    GameObject obj1, obj2, lm;
+    LevelManager levelManager;
+    Sprite sp;
+    Texture2D txt;
+    public Image image1, image2;
+
     void Start()
     {
-        renderer = gameObject.GetComponent<SpriteRenderer>();
-        transform = gameObject.GetComponent<Transform>();
-       
+
+
+        obj1 = GameObject.FindWithTag("BlueCup");
+        obj2 = GameObject.FindWithTag("OrangeCup");
+        lm = GameObject.FindGameObjectWithTag("LevelManager");
+        levelManager = lm.GetComponent<LevelManager>();
+
+        image1 = obj1.GetComponent<Image>();
+        image2 = obj2.GetComponent<Image>();
+
     }
     void Update()
     {
-        
-        if (AmmountCollected > 0 && AmmountCollected <= 2)
+       AmmountCollectedBlue = levelManager.getBlueScore();
+       AmmountCollectedOrange = levelManager.getRedScore();
+
+
+        if (AmmountCollectedBlue > 0 && AmmountCollectedBlue <= 2)
         {
-            renderer.sprite = newSprite1;
-           
+            image1.sprite = newSprite1;
         }
-        else if (AmmountCollected > 2 && AmmountCollected <= 4)
+        else if (AmmountCollectedBlue > 2 && AmmountCollectedBlue <= 4)
         {
-            renderer.sprite = newSprite2;
+            image1.sprite = newSprite2;
         }
-        else if (AmmountCollected > 4 && AmmountCollected <= 6)
+        else if (AmmountCollectedBlue > 4 && AmmountCollectedBlue <= 6)
         {
-           
-            renderer.sprite = newSprite3;
-           
+            image1.sprite = newSprite3;
         }
-        else if (AmmountCollected > 6 && AmmountCollected <= 8)
+        else if (AmmountCollectedBlue > 6 && AmmountCollectedBlue <= 8)
         {
-            
-            renderer.sprite = newSprite4;
+            image1.sprite = newSprite4;
         }
-        else if (AmmountCollected > 8 && AmmountCollected <= 10)
+        else if (AmmountCollectedBlue > 8 && AmmountCollectedBlue <= 10)
         {
-           
-            renderer.sprite = newSprite5;
+            image1.sprite = newSprite5;
         }
-         lastAmmount = AmmountCollected;
+
+
+
+        if (AmmountCollectedOrange > 0 && AmmountCollectedOrange <= 2)
+        {
+            image2.sprite = newSprite6;
+        }
+        else if (AmmountCollectedOrange > 2 && AmmountCollectedOrange <= 4)
+        {
+            image2.sprite = newSprite7;
+        }
+        else if (AmmountCollectedOrange > 4 && AmmountCollectedOrange <= 6)
+        {
+            image2.sprite = newSprite8;
+        }
+        else if (AmmountCollectedOrange > 6 && AmmountCollectedOrange <= 8)
+        {
+            image2.sprite = newSprite9;
+        }
+        else if (AmmountCollectedOrange > 8 && AmmountCollectedOrange <= 10)
+        {
+            image2.sprite = newSprite10;
+        }
+
+
+
     }
-    
- 
 }
--- GlobalGameJamFishGame/Assets/Scripts/Balance.cs
+++ GlobalGameJamFishGame/Assets/Scripts/Balance.cs
@@ -8,7 +8,7 @@ public class Balance : MonoBehaviour
    GameObject Blue ;
     GameObject Orange;
     private AmmountCheck AC1, AC2;
-    SpriteRenderer renderer;
+    SpriteRenderer render;
     Vector3 pos1, pos1low,pos1high;
     Vector3 pos2,pos2low,pos2high;
 
@@ -20,7 +20,8 @@ public class Balance : MonoBehaviour
        Orange = GameObject.Find("OrangeCup");
         AC1 = Blue.GetComponent<AmmountCheck>();
        AC2 = Orange.GetComponent<AmmountCheck>();
-       renderer = gameObject.GetComponent<SpriteRenderer>();
+       render = gameObject.GetComponent<SpriteRenderer>();
+
         pos1 = AC1.transform.position;
         pos2 = AC2.transform.position;
         pos1low= pos1 + new Vector3(0, -10, 0);
@@ -32,29 +33,29 @@ public class Balance : MonoBehaviour
     
     void Update()
     {
-        if (AC1.AmmountCollected==AC2.AmmountCollected)
+        if (AC1.AmmountCollectedBlue==AC2.AmmountCollectedOrange)
         {
-            renderer.sprite = newSprite1;
+            render.sprite = newSprite1;
             AC1.transform.position = pos1;
             AC2.transform.position = pos2;
 
         }
-        else if (AC1.AmmountCollected > AC2.AmmountCollected)
+        else if (AC1.AmmountCollectedBlue > AC2.AmmountCollectedOrange)
         {
            
                 AC1.transform.position =pos1high;
                 AC2.transform.position = pos2low;
                 
-            renderer.flipX = true;
-            renderer.sprite = newSprite2;
+            render.flipX = true;
+            render.sprite = newSprite2;
         }
-        else if (AC1.AmmountCollected < AC2.AmmountCollected)
+        else if (AC1.AmmountCollectedBlue < AC2.AmmountCollectedOrange)
         {
            
                 AC2.transform.position = pos2high;
                 AC1.transform.position = pos1low;
-            renderer.flipX = false;
-            renderer.sprite = newSprite3;
+            render.flipX = false;
+            render.sprite = newSprite3;
         }
 
     }
