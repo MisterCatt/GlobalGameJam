d5680d4930cb17e5675be2497a278b192229447c
Daniel Mardunovich <mardunovich@gmail.com>
2022-01-29 06:30:57 +0100

    adding ui

---
+++ GlobalGameJamFishGame/Assets/Scripts/UiScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject levelManager, text;
+    LevelManager lm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lm = levelManager.GetComponent<LevelManager>();
+        text.GetComponent<Text>().text = "Red score: " + lm.getRedScore() + "\nBlue score: " + lm.getBlueScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.GetComponent<Text>().text = "Red score: " + lm.getRedScore() + "\nBlue score: " + lm.getBlueScore();
+    }
+}
