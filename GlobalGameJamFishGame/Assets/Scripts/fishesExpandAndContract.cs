ccef966f8d71da0f6149a93f062b1960ae9dc7a8
Daniel Mardunovich <mardunovich@gmail.com>
2022-01-29 03:34:12 +0100

    adding up and down movement

--- GlobalGameJamFishGame/Assets/Scripts/fishesExpandAndContract.cs
+++ GlobalGameJamFishGame/Assets/Scripts/fishesExpandAndContract.cs
@@ -7,10 +7,16 @@ public class fishesExpandAndContract : MonoBehaviour
     public float maxRadius;
     public float minRadius;
     public float radiusChangeSpeed;
+    public float upDownSpeed;
+
     public GameObject fish1;
     public GameObject fish2;
+
     public KeyCode contractingButton;
     public KeyCode expandingButton;
+    public KeyCode up;
+    public KeyCode down;
+
     public float rotationSpeed;
     public float temporaryRadius;
     Vector3 temporaryRotation;
@@ -18,6 +24,7 @@ public class fishesExpandAndContract : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        upDownSpeed = 0.05f;
         temporaryRadius = Mathf.Sqrt(Mathf.Pow(fish1.transform.position.x - fish2.transform.position.x, 2) + Mathf.Pow(fish1.transform.position.y - fish2.transform.position.y ,2));
         temporaryRotation = transform.rotation.eulerAngles;
     }
@@ -47,6 +54,14 @@ public class fishesExpandAndContract : MonoBehaviour
 
 
             }
+        }
+
+        if (Input.GetKey(up))
+        {
+            transform.position = new Vector3(0,transform.position.y+upDownSpeed,0);
+        }else if (Input.GetKey(down))
+        {
+            transform.position = new Vector3(0, transform.position.y- upDownSpeed,0);
         }
         temporaryRotation.z -= rotationSpeed;
         transform.rotation = Quaternion.Euler(temporaryRotation);
